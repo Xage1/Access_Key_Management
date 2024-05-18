f9e76a11027949233b6649757ec29a5424f5ec61
Ayine-nongre <eugeneatinbire@gmail.com>
2024-05-18 09:27:05 +0000

    Setting dev enviroment

---
+++ app.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+
+const app = express()
+dotenv.config()
+
+const port = process.env.PORT || 3000
+
+// set up database connection
+mongoose.connect(process.env.DATABASE_URL);
+
+
+var set = mongoose.connection;
+set.on('error', console.error.bind(console, 'connection error:'));
+set.once('open', function() {
+    console.log('Db connected successfully')
+});
+
+// setting up middleware to parse body data
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`)
+})
