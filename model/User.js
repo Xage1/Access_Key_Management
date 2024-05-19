e8699a49b2b57cb922ee5f42b196860a641d3eef
Ayine-nongre <eugeneatinbire@gmail.com>
2024-05-19 12:44:01 +0000

    Adding user schema

--- model/User.js
+++ model/User.js
@@ -6,7 +6,6 @@ export const User = mongoose.model('User', {
     role: { type: String, required: true, enum: ['admin', 'personnel'], default: 'personnel' },
     status: { type: String, required: true, default: 'inactive' },
     activation_token: { type: String, required: true },
-    reset_OTP: { type: String },
     key: [{
         type: mongoose.Schema.ObjectId,
         ref: 'Key'
