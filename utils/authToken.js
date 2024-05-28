0dc1c36709731ab7be16f6d07cd4af62bd01856f
holladworld <oladimejiolayinka95@gmail.com>
2024-05-28 16:59:47 +0100

    Remove duplicate code line

--- utils/authToken.js
+++ utils/authToken.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 
 // Function to generate JWT token using user's email, role, and status
-import jwt from 'jsonwebtoken';
+
 
 export const generateToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, {
