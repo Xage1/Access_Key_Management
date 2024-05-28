eb319cc218d299be7dc5f1d8cee5cf508304b5c2
Kenneth Muketha <89647552+Xage1@users.noreply.github.com>
2024-05-28 12:33:51 +0300

    Create verifyUser.js for user verification using a token

---
+++ controllers/verifyUser.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+
+router.get('/verify', async (req, res) => {
+  try {
+    const { token } = req.query;
+
+    if (!token) {
+      return res.status(400).json({ error: 'Activation token is missing' });
+    }
+    // Find the user by activation token
+    const user = await User.findOne({ activationToken: token });
+
+    //Check if user with given activation token exists
+    if (!user) {
+      return res.status(404).json({ error: 'Invalid activation token' });
+    }
+    //Update user's account status to active
+    user.active = true;
+    user.activationToken = null;
+    await user.save();
+
+    return res.status(200).json({ message: 'User account verified successfully' });
+  } catch (error) {
+    console.error('Error verifying user:', error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+module.exports = router;
